1b24f297762add6d97a3633efb3b0e8a9eb22cc1
yuji <yuji-morita@united.jp>
2016-04-19 01:04:45 +0000

    submit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(7)
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(7)
+    end
+    
   end
 end
