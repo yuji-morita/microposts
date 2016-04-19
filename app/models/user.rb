a9ad3d7a743c0c6102988b246d4c326fae9b730e
yuji <yuji-morita@united.jp>
2016-04-19 00:04:23 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,14 +29,13 @@ class User < ActiveRecord::Base
     following_relationship = following_relationships.find_by(followed_id: other_user.id)
     following_relationship.destroy if following_relationship
   end
-  
-  def feed_items
-    Micropost.where(user_id: following_user_ids + [self.id])
-  end
-  
 
   # あるユーザーをフォローしているかどうか？
   def following?(other_user)
     following_users.include?(other_user)
+  end
+  
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
   end
 end
